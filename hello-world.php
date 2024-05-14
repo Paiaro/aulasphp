01e0eb37f15773795a3bb1c43a9f66355b9b61d2
Paiaro <gabrielpaiarop@gmail.com>
2024-05-14 17:12:20 -0300

    Update hello-world.php

--- hello-world.php
+++ hello-world.php
@@ -1,6 +1,15 @@
-<?php
-
-echo "hello world";
-
-
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aulas PHP 01</title>
+</head>
+<body>
+    <h1>Aulas PHP 14/05</h1>
+    <hr>
+    <?php
+   echo "JHAFGAIGFAHFVAGKHGFALJFHOFAHFOAFHAOFH"; 
+    ?>
+</body>
+</html>
