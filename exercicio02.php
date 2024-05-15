e89a8c8114517b1def34522f2d56459ba146a939
Paiaro <gabrielpaiarop@gmail.com>
2024-05-15 17:19:29 -0300

    Update exercicio02.php

--- exercicio02.php
+++ exercicio02.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicio 02</title>
+    <style>
+       h1,ol{text-align: center;}
+    </style>
 </head>
 
 <body>
