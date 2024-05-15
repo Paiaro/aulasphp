bae1ac3184645ccfa37ae6450f5ded7a5c6f100e
Paiaro <gabrielpaiarop@gmail.com>
2024-05-15 15:10:59 -0300

    Update exercicio01.php

--- exercicio01.php
+++ exercicio01.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicio 01 php 15/05</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
   .nome {
       font-weight: bold;
@@ -24,8 +25,8 @@
 $nome = 'Chapolin';
 $idade = 25;
 ?>
-<p> <span class = "nome"><?=$nome?></span> possui <span class = "idade"><?=$idade?></span>.</p>
+<p> <span class = "nome alert alert-primary" role="alert"><?=$nome?></span> possui <span class = "idade alert alert-primary" role="alert"><?=$idade?></span> anos.</p>
 
-    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
