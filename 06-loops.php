f06b897599e93f2803ba978c9b68f94af7d67f8e
Paiaro <gabrielpaiarop@gmail.com>
2024-05-16 16:42:26 -0300

    Update 06-loops.php

--- 06-loops.php
+++ 06-loops.php
@@ -1,34 +1,45 @@
 <!DOCTYPE html>
 <html lang="pt-bt">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Loops PHP</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
-        p{
+        p {
             text-align: center;
         }
     </style>
 </head>
+
 <body>
-<div class="container">
+    <div class="container">
 
-    <h1>Loops no PHP</h1>
-<hr>
-<h2>Tradicionais: <code>while e for</code></h2>
+        <h1>Loops no PHP</h1>
+        <hr>
+        <h2>Tradicionais: <code>while e for</code></h2>
 
-<?php
-$i = 1;
-while($i <=3){ ?>
-<p class="alert alert-info">Senac Penha</p>
+        <?php
+        $i = 1;
+        while ($i <= 3) { ?>
+            <p class="alert alert-info">Senac Penha</p>
 
-<?php
-$i++;
-}
-?>
-
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <?php
+            $i++;
+        }
+        ?>
+        <h2>Exemplo com <code>for</code>:</h2>
+        <hr>
+        <?php
+        for ($i = 1; $i <= 5; $i++) {
+        ?>
+            <p class="alert alert-warning">exemplo <?= $i ?></p>
+        <?php
+        }
+        ?>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
