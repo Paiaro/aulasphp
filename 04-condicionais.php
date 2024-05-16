b075503873eac126513bde7d54fe44dcee1f241b
Paiaro <gabrielpaiarop@gmail.com>
2024-05-16 13:53:52 -0300

    Create 04-condicionais.php

---
+++ 04-condicionais.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>04 - Condicionais</title>
+</head>
+<body>
+    <h1>Condicionais no PHP</h1>
+    <hr>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+</body>
+</html>
