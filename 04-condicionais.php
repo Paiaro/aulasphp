0e34cafae75657ce98885bac6dbf829fb86f8ee3
Paiaro <gabrielpaiarop@gmail.com>
2024-05-16 13:59:38 -0300

    Update 04-condicionais.php

--- 04-condicionais.php
+++ 04-condicionais.php
@@ -4,10 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>04 - Condicionais</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <h1>Condicionais no PHP</h1>
     <hr>
+<h2>Condicionais Simples (não tem o <code>else</code>)</h2>
+
+<?php
+$numero = 50;
+if($numero < 100){
+
+    echo "<p>$numero é verdadeira/true!</p>";
+
+}
+
+
+
+?>
 
 
 
@@ -21,7 +35,6 @@
 
 
 
-
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
