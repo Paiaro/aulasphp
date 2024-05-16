5723589d6d075621f8aa5af53c153c46a5ca50c3
Paiaro <gabrielpaiarop@gmail.com>
2024-05-16 16:27:40 -0300

    Update 06-loops.php

--- 06-loops.php
+++ 06-loops.php
@@ -7,9 +7,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+<div class="container">
+
     <h1>Loops no PHP</h1>
 <hr>
-<div class="container">
 
 
 
