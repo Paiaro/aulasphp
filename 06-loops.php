2d30f5a58475bca43a946b798488ffa6df945f2c
Paiaro <gabrielpaiarop@gmail.com>
2024-05-16 16:46:17 -0300

    Update 06-loops.php

--- 06-loops.php
+++ 06-loops.php
@@ -38,6 +38,11 @@
         <?php
         }
         ?>
+<hr>
+        <h3><code>for </code>com <code>array</code></h3>
+    <?php
+    $alunos = ["chaves","chapolin","kiko"];
+    ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
