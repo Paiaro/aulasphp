3e5b49ec306f5a3c7dbd3b71f730a61ae4389598
Paiaro <gabrielpaiarop@gmail.com>
2024-05-17 10:03:13 -0300

    Update exercicio-03.php

--- exercicio-03.php
+++ exercicio-03.php
@@ -4,22 +4,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>exercicio - 03 php</title>
+    <title>Exercício 03 PHP</title>
+    <style>
+        .blue {
+            color: blue;
+        }
+
+        .red {
+            color: red;
+        }
+    </style>
 </head>
 
 <body>
-    <h1>exercicio 03 PHP</h1>
+    <h1>Exercício 03 PHP</h1>
     <hr>
     <ol>
         <?php
         $meses = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"];
-        $i = 0;
-        foreach ($meses as $mes) {
-            $cor = $i % 2 == 0 ? 'blue' : 'red';
-            echo "<li style='color: $cor;'>$mes</li>";
-            $i++;
-        }
         ?>
+        <?php foreach ($meses as $index => $mes): ?>
+            <li class="<?= $index % 2 == 0 ? 'blue' : 'red'; ?>">
+                <?= htmlspecialchars($mes, ENT_QUOTES, 'UTF-8') ?>
+            </li>
+        <?php endforeach; ?>
     </ol>
 </body>
 
