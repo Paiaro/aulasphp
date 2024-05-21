ef90746d7d826ffe17c331dcdd123b963a8de7e6
Paiaro <gabrielpaiarop@gmail.com>
2024-05-21 15:10:32 -0300

    Create index.php

---
+++ site/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site</title>
+</head>
+
+<body>
+    <header>
+        <h1>site exemplo</h1>
+        <nav><a href="index.php">Home</a>
+            <a href="equipe.php">Equipe</a>
+            <a href="portifolio.php">Portifolio</a>
+        </nav>
+    </header>
+    <main>
+        <article>
+            <h2>Bem vindo(a)</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex adipisci deleniti odit est dolorum? At, tempora! Exercitationem natus sunt vitae quasi, eum, voluptates reprehenderit ut laudantium necessitatibus est quibusdam voluptatibus.</p>
+        </article>
+    </main>
+
+    <footer>
+
+        <h2>site criado como exemplo</h2>
+
+    </footer>
+</body>
+
+</html>
