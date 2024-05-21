b569cb99fd8609a9fce87a9b6bbe57e43fcfb2e8
Paiaro <gabrielpaiarop@gmail.com>
2024-05-21 16:15:37 -0300

    ....

--- site/index.php
+++ site/index.php
@@ -1,32 +1,8 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site</title>
-</head>
-
-<body>
-    <header>
-        <h1>site exemplo</h1>
-        <nav><a href="index.php">Home</a>
-            <a href="equipe.php">Equipe</a>
-            <a href="portfolio.php">Portfólio</a>
-        </nav>
-    </header>
-    <main>
-        <article>
+<?php 
+include "cabecalho.php";
+?>
             <h2>Bem vindo(a)</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex adipisci deleniti odit est dolorum? At, tempora! Exercitationem natus sunt vitae quasi, eum, voluptates reprehenderit ut laudantium necessitatibus est quibusdam voluptatibus.</p>
-        </article>
-    </main>
-
-    <footer>
-
-        <h2>site criado como exemplo</h2>
-
-    </footer>
-</body>
-
-</html>
+   <?php
+   include "rodape.php";
+   ?>
---
+++ site/rodape.php
@@ -0,0 +1,11 @@
+</article>
+    </main>
+
+    <footer>
+
+        <h2>site criado como exemplo</h2>
+
+    </footer>
+</body>
+
+</html>
