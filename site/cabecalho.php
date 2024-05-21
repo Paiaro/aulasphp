efbf8535c2ff2d507d3af15e47bf5fe3d21b029f
Paiaro <gabrielpaiarop@gmail.com>
2024-05-21 17:18:14 -0300

    Update cabecalho.php

--- site/cabecalho.php
+++ site/cabecalho.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo isset($pageTitle) ? $pageTitle : 'Meu Site'; ?></title>
+    <title><?= isset($pageTitle) ? $pageTitle : 'Meu Site'; ?></title>
 </head>
 
 <body>
