dd5d30f0ae82afa19df3ff9a4fbcf119e9b061e6
Paiaro <gabrielpaiarop@gmail.com>
2024-05-21 14:48:50 -0300

    php include

--- hello-world.php
+++ hello-world.php
@@ -1,9 +1,12 @@
+<?php
+include "recursos.php";
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>aulas PHP 01</title>
+    <title><?=EMPRESA?> aulas PHP 01</title>
 </head>
 <body>
     <h1>Aulas PHP 14/05</h1>
---
+++ recursos.php
@@ -0,0 +1,18 @@
+<?php
+//recursos.php
+
+const EMPRESA = "ABC Tecnologia";
+$anoFiscal = 2024;
+$tecnologias = ["Python", "IA Generativa","machine learning"];
+
+
+
+
+
+
+
+
+
+
+
+// em arquivos so de php não precisa de fechamento "? >"
