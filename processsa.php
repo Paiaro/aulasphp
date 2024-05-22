72e16d053cacf6587bcc36cf947c411a30c07a66
Paiaro <gabrielpaiarop@gmail.com>
2024-05-22 14:20:43 -0300

    Update processsa.php

--- processsa.php
+++ processsa.php
@@ -1,19 +1,24 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>processando os dados</title>
 </head>
+
 <body>
     <h1>Processamento dos dados vindos do formulário</h1>
     <hr>
 
     <?php
     // capturando dados prevenientes do formulário usando o array superglobal $_POST[] (se o form usar o metodo get, então usariamos $_GET[]).
-     
-
-
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $nascimento = $_POST["nascimento"];
+    $mensagem = $_POST["mensagem"];
     ?>
+
 </body>
+
 </html>
